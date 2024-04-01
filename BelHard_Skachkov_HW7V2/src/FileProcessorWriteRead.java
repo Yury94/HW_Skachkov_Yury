2a42b07e750375e561ab62243c5b782c4815a2b8
Yury <skachkovyury94@gmail.com>
2024-04-01 21:56:52 +0300

    Added change of code in FileProcessorWriteRead & MenuAddNewObject files HW7. Reading data from operation memory now!

--- BelHard_Skachkov_HW7V2/src/FileProcessorWriteRead.java
+++ BelHard_Skachkov_HW7V2/src/FileProcessorWriteRead.java
@@ -1,23 +1,8 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class FileProcessorWriteRead {
-    public static void writingData() throws IOException {
-        ArrayList<Transport> unitCar = new ArrayList();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter parameters new object:\n Enter power car:  ");
-        int power = scanner.nextInt();
-        System.out.println("Enter maxspeed car: ");
-        int maxspeed = scanner.nextInt();
-        System.out.println("Enter weight car: ");
-        int weight = scanner.nextInt();
-        System.out.println("Enter brend car: \nChoose brend: 1. BMW, 2. Honda, 3. Mazda");
-        Scanner scanner1 = new Scanner(System.in);
-        String brend = scanner1.nextLine();
-        unitCar.add(new Transport(power, maxspeed, weight, brend));
+    public static void writingData(ArrayList<Transport> unitCar) throws IOException {
         FileWriter writer = new FileWriter("src/resources/FileWriter.txt", false);
         writer.write(String.valueOf(unitCar));
         writer.write("\n");
@@ -25,10 +10,11 @@ public class FileProcessorWriteRead {
     }
 
     public static void readingData() {
-        try (FileReader reader = new FileReader("src/resources/FileWriter.txt")) {
-            int i;//считываем каждый байт в переменную i
-            while ((i = reader.read()) != -1) {
-                System.out.print((char) i);//
+        try (FileInputStream reader = new FileInputStream("src/resources/FileWriter.txt");
+             BufferedReader buffered = new BufferedReader(new InputStreamReader(reader))) {//читаем из оперативы
+            String i;
+            while ((i = buffered.readLine()) != null) {//считываем каждый байт в переменную i до тех пор пока i не станет пустым
+                System.out.print(i + "test");//добавил приставку test чтобы понимать, что я читаю из оперативной памяти
             }
         } catch (IOException ex) {
             throw new RuntimeException();
